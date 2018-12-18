d7a74982ee22fd6e1d73f1aff17bbec9b372f38d
Richard Bellamy <richard.bellamy123@gmail.com>
2018-12-17 18:48:52 -0800

    got meetups n db working

--- schema.sql
+++ schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS weathers;
 DROP TABLE IF EXISTS locations;
+DROP TABLE IF EXISTS meetups;
 
 CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
@@ -13,6 +14,18 @@ CREATE TABLE weathers (
     id SERIAL PRIMARY KEY,
     forecast VARCHAR(255),
     time VARCHAR(255),
+    created_at NUMERIC,
     location_id INTEGER NOT NULL,
     FOREIGN KEY (location_id) REFERENCES locations (id) 
+);
+
+CREATE TABLE meetups (
+    id SERIAL PRIMARY KEY,
+    link VARCHAR(255),
+    name VARCHAR(255),
+    creation_date NUMERIC,
+    host VARCHAR(255),
+    created_at NUMERIC,
+    location_id INTEGER NOT NULL,
+    FOREIGN KEY (location_id) REFERENCES locations (id)
 );
